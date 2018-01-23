14a0d491430ef79b23af98ce3fb9f854114c8912
pavlvs <pavlvsx@gmail.com>
2018-01-23 18:12:12 +0100

    Implement removal of single task, clearing all tasks and filtering tasks.

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
 	// add task event
 	form.addEventListener('submit', addTask);
+	// remove task event
+	taskList.addEventListener('click', removeTask);
+	// Clear tasks event
+	clearBtn.addEventListener('click', clearTasks);
+	// Task filter event
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -40,10 +46,31 @@ function addTask(e) {
 	//Clear the task input
 	taskInput.value = '';
 
-
-
-
-
-
 	e.preventDefault();
+}
+// remove task
+function removeTask(e) {
+	if (e.target.parentElement.classList.contains('delete-item')) {
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+		}
+	}
+}
+// clear tasks
+function clearTasks() {
+	while (taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
+	}
+}
+// filter tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+	document.querySelectorAll('.collection-item').forEach(function(task) {
+		const item = task.firstChild.textContent;
+		if (item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
 }
